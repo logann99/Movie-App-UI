6b8877e23652f01ed88dfa99cb9d6cac3f9247e1
logann99 <ahmed.201317499@gmail.com>
2023-05-10 14:42:02 +0300

    Movie App UI part3

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:movie_app/components/custom_outline.dart';
 import 'package:movie_app/constants.dart';
 
 class OnBoardingScreen extends StatelessWidget {
@@ -32,7 +33,7 @@ class OnBoardingScreen extends StatelessWidget {
                   sigmaX: 200,
                   sigmaY: 200,
                 ),
-                child:  Container(
+                child: const SizedBox(
                   height: 166,
                   width: 166,
                 ),
@@ -43,8 +44,8 @@ class OnBoardingScreen extends StatelessWidget {
             top: screenHight * 0.3,
             right: -100,
             child: Container(
-              height: 166,
-              width: 166,
+              height: 200,
+              width: 200,
               decoration: const BoxDecoration(
                 shape: BoxShape.circle,
                 color: Constants.kGreenColor,
@@ -54,11 +55,81 @@ class OnBoardingScreen extends StatelessWidget {
                   sigmaX: 200,
                   sigmaY: 200,
                 ),
-                child:  Container(
+                child: const SizedBox(
                   height: 200,
                   width: 200,
                 ),
               ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: screenHight * 0.07,
+                ),
+                CustomOutline(
+                  strokeWidth: 4,
+                  radius: screenWidth * 0.8,
+                  padding: const EdgeInsets.all(
+                    4,
+                  ),
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Constants.kPinkColor,
+                      Constants.kPinkColor.withOpacity(0),
+                      Constants.kGreenColor.withOpacity(0.1),
+                      Constants.kGreenColor,
+                    ],
+                    stops: const [0.2, 0.4, 0.6, 1],
+                  ),
+                  width: screenWidth * 0.8,
+                  height: screenWidth * 0.8,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/img-onboarding.png',
+                        ),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.bottomLeft,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHight * 0.09,
+                ),
+                Text(
+                  'Watch Movies in \n Virtual Reality',
+                  style: TextStyle(
+                    fontSize: screenHight <= 660 ? 18 : 34,
+                    color: Constants.kWhiteColor.withOpacity(
+                      0.8,
+                    ),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHight * 0.05,
+                ),
+                Text(
+                  'Download and Watch Offline in \n Whenever You Are',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: screenHight <= 660 ? 12 : 16,
+                    color: Constants.kWhiteColor.withOpacity(
+                      0.8,
+                    ),
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+              ],
             ),
           ),
         ]),
