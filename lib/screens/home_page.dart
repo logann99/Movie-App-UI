ed1939a5c11c48a04f2b4be4ac27a750ad3dcea2
logann99 <ahmed.201317499@gmail.com>
2023-05-11 16:12:43 +0300

    Movie App UI Part7

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,12 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:glassmorphism/glassmorphism.dart';
+import 'package:movie_app/components/masked_image.dart';
 import 'package:movie_app/components/search_field_widget.dart';
 import 'package:movie_app/constants.dart';
+import 'package:movie_app/models/movie.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -92,11 +96,11 @@ class HomePage extends StatelessWidget {
               child: ListView(
                 primary:
                     true, //Creates a scrollable, linear array of widgets from an explicit
-                children: const [
-                  SizedBox(
+                children: [
+                  const SizedBox(
                     height: 24,
                   ),
-                  Text(
+                  const Text(
                     'What Would You \n Like To Watch.?',
                     textAlign: TextAlign.center,
                     style: TextStyle(
@@ -105,18 +109,223 @@ class HomePage extends StatelessWidget {
                       fontWeight: FontWeight.w700,
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 30,
                   ),
-                  SearchFieldWidget(
+                  const SearchFieldWidget(
                     padding: EdgeInsets.symmetric(
                       horizontal: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 39,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 20.0,
+                    ),
+                    child: Text(
+                      'Popular Movies',
+                      style: TextStyle(
+                        color: Constants.kWhiteColor,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 37,
+                  ),
+                  //Popular Moviessssssssssssssssss
+                  SizedBox(
+                    height: 160,
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: newMovies.length,
+                      itemBuilder: ((context, index) {
+                        String mask;
+                        if (index == 0) {
+                          mask = Constants.kMaskFirstIndex;
+                        } else if (index == newMovies.length - 1) {
+                          mask = Constants.kMaskLastIndex;
+                        } else {
+                          mask = Constants.kMaskCenter;
+                        }
+                        return GestureDetector(
+                          child: Container(
+                            margin: const EdgeInsets.only(
+                              left: 20,
+                            ),
+                            height: 160,
+                            width: 142,
+                            child: MaskedImage(
+                              asset: newMovies[index].moviePoster,
+                              mask: mask,
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 38,
+                  ),
+                  //Up Commingggggggggggggggggggg
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 20.0,
+                    ),
+                    child: Text(
+                      'Up Comming Movies',
+                      style: TextStyle(
+                        color: Constants.kWhiteColor,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 37,
+                  ),
+                  SizedBox(
+                    height: 160,
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: upcomingMovies.length,
+                      itemBuilder: ((context, index) {
+                        String mask;
+                        if (index == 0) {
+                          mask = Constants.kMaskFirstIndex;
+                        } else if (index == upcomingMovies.length - 1) {
+                          mask = Constants.kMaskLastIndex;
+                        } else {
+                          mask = Constants.kMaskCenter;
+                        }
+                        return GestureDetector(
+                          child: Container(
+                            margin: const EdgeInsets.only(
+                              left: 20,
+                            ),
+                            height: 160,
+                            width: 142,
+                            child: MaskedImage(
+                              asset: upcomingMovies[index].moviePoster,
+                              mask: mask,
+                            ),
+                          ),
+                        );
+                      }),
                     ),
                   ),
                 ],
               ),
             ),
           ],
+        ),
+      ),
+      floatingActionButton: Container(
+        height: 64,
+        width: 64,
+        padding: const EdgeInsets.all(
+          4,
+        ),
+        margin: const EdgeInsets.only(
+          top: 40,
+        ),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Constants.kPinkColor.withOpacity(0.2),
+              Constants.kGreenColor.withOpacity(0.2),
+            ],
+          ),
+        ),
+        child: Container(
+          height: 60,
+          width: 60,
+          padding: const EdgeInsets.all(
+            4,
+          ),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Constants.kPinkColor.withOpacity(0.2),
+                Constants.kGreenColor.withOpacity(0.2),
+              ],
+            ),
+          ),
+          child: RawMaterialButton(
+            onPressed: () {},
+            shape: const CircleBorder(),
+            fillColor: const Color(0xff404c57),
+            child: SvgPicture.asset(Constants.kIconPlus),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: GlassmorphicContainer(
+        height: 92,
+        width: screenWidth,
+        borderRadius: 0,
+        linearGradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Constants.kWhiteColor.withOpacity(0.1),
+            Constants.kWhiteColor.withOpacity(0.1),
+          ],
+        ),
+        blur: 30,
+        border: 0,
+        borderGradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Constants.kPinkColor,
+            Constants.kGreenColor,
+          ],
+        ),
+        child: BottomAppBar(
+          elevation: 0, //for hide icon shadow
+          notchMargin: 4, //for hide icon shadow
+          color: Colors.transparent,
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Expanded(
+              child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(Constants.kIconHome),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(Constants.kIconPlayOnTv),
+              ),
+            ),
+            const Expanded(
+              child: Text(' '),
+            ),
+            Expanded(
+              child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(Constants.kIconCategories),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(Constants.kIconDownload),
+              ),
+            ),
+          ]),
         ),
       ),
     );
